1ac923b5280c2ee111a81e45eb8aa5b3dad43cf3
dasmaihar2017 <110130561+dasmaihar2017@users.noreply.github.com>
2025-09-13 21:08:51 +0530

    Add files via upload

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'message' => 'Laravel test deployment successful!'
+        ]);
+    }
+}
